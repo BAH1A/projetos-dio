5064699ab6cb1b28bf785dc0e39419d6eecc31ce
BAH1A <91296728+BAH1A@users.noreply.github.com>
2023-06-19 12:35:20 -0300

    Atualização

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -9,21 +9,22 @@ public class Contador {
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
-        System.out.println(parametroUm+parametroDois);
-	/*
+	
 		try {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}//catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-		//}
+		}catch (? exception) {
+			imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem*/
+		//realizar o for para imprimir os números com base na variável contagem
 	}
 }
+
+/*https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/controle-fluxo */
