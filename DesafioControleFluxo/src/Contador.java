099af416381a74993cc19750e8f49526a2e4bf1e
BAH1A <91296728+BAH1A@users.noreply.github.com>
2023-06-20 05:24:18 -0300

    atualização

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -11,20 +11,31 @@ public class Contador {
 		int parametroDois = terminal.nextInt();
 	
 		try {
-			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
-		
-		}catch (? exception) {
-			imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 		}
-		
+		catch (ParametrosInvalidosException exception) {
+			 System.out.println("O segundo número deve ser maior que o primeiro número.");
+		}
 	}
+
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm >= parametroDois);
 		
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+			for (int i = 1; i <= contagem; i = i + 1) {
+
+            	System.out.println("Imprimindo o número " + i);
+        	}
 	}
 }
 
 /*https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/controle-fluxo */
+/**
+ * 	Criar um projeto que faça uma contagem.
+ * 	O terminal vai passar 2 números e que será jogados em 2 variáveis.
+ * 	Com estes dois números você deverá obter a quantidade de interações (for) e realizar a impressão no 
+ * 	console (System.out.print) dos números incrementados, exemplo: "Imprimindo o número 1", "Imprimindo o número 2" e assim por diante.
+ * 	Caso a variável 1 > 2 deve cair na excessão customisada informando "O segundo parâmetro deve ser maior que o 
+ * 	primeiro"
+ */
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,3 +1,7 @@
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception {
+    public static void main(String[] args) {
+        
+        return;
     
+    }    
 }
